3573a797ef8eb63db8630827ebed791fd39db2ec
Kenath Malruwan <115833643+Kenath526@users.noreply.github.com>
2023-07-31 00:13:07 +0530

    Lab 01

---
+++ Lab 01/Lab 01 Q01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_Q01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Enter your name:");
+            name = Console.ReadLine();
+            string batch;
+            Console.WriteLine("Enter your batch :");
+            batch = Console.ReadLine();
+
+            Console.WriteLine("Hi " + name);
+            Console.WriteLine("Your are from batch " + batch);
+            Console.ReadKey();
+
+           
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab 01/Lab 01 Q02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lan_01_Q02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       
+        
+            {
+                Console.Write("Enter the radius of the circle: ");
+                double radius = Convert.ToDouble(Console.ReadLine());
+                double area = Math.PI * Math.Pow(radius, 2);
+                Console.WriteLine("The area of the circle is: " + area);
+            }
+        }
+    }
+
+}
+    
+
