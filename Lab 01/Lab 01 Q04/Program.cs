032df0e179c15b57ac76d28c2715da5d5f6ec513
Kenath Malruwan <115833643+Kenath526@users.noreply.github.com>
2023-07-31 14:13:57 +0530

    Update Program.cs

--- Lab 01/Lab 01 Q04/Program.cs
+++ Lab 01/Lab 01 Q04/Program.cs
@@ -10,6 +10,16 @@ namespace Lab_01_Q04
     {
         static void Main(string[] args)
         {
+                Console.Write("Enter employee salary: ");
+                double salary = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter tax rate: ");
+                double taxRate = Convert.ToDouble(Console.ReadLine());
+
+                double tax = salary * taxRate;
+                double salaryAfterTax = salary - tax;
+
+                Console.WriteLine("Salary after tax: " + salaryAfterTax);
         }
     }
 }
